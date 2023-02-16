cf64625a6e7c4763722d6173b7060d6d11e069c7
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-02-16 15:50:23 +0100

    [edit] - memcpy.asm - change algo #11

--- memcpy.asm
+++ memcpy.asm
@@ -7,18 +7,24 @@ memcpy:
     ENTER 0, 0
     XOR RAX, RAX
     MOV RAX, 0
+    PUSH RDI
+    XOR R8, R8
+    XOR RCX, RCX
     MOV RCX, 0
 
 loop:
     CMP BYTE [RSI], 0
+    JE end 
+    CMP RDX, RCX
     JE end
-    CMP RCX, RDX
-    JE end
-    MOVSB               ;
+    MOV R8B, BYTE [RSI]
+    MOV [RDI + RCX], R8B
     INC RCX
+    INC RSI
     JMP loop
 
 end:
+    MOV RAX, RDI
     LEAVE
     RET
 
