095fb4d82d46b955f36074b9246f523a21a88693
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-02-21 22:17:13 +0100

    [add] - memmove.asm - add memmove file - #5

---
+++ memmove.asm
@@ -0,0 +1,38 @@
+BITS 64
+section .text
+
+GLOBAL memmove
+
+memmove:
+    ENTER 0, 0
+    XOR RAX, RAX
+    MOV RAX, 0
+    XOR R8, R8
+    XOR R9, R9
+    XOR RCX, RCX
+    MOV RCX, 0
+
+    CMP RDX, RCX
+    JE end
+    CMP RDI, 0
+    JE void
+    CMP RSI, 0
+    JE void
+
+loop:
+    CMP RDX, RCX
+    JE end
+    MOV R8B, BYTE [RSI]
+    MOV [RDI + RCX], R8B
+    INC RCX
+    INC RSI
+    JMP loop
+
+void:
+    LEAVE
+    RET
+
+end:
+    MOV RAX, RDI
+    LEAVE
+    RET
